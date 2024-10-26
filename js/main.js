7973dffcd867a4b244da3502d5dd43dfb239e3b6
WahyuAji11 <alhafidzw1@gmail.com>
2024-10-27 01:44:44 +0800

    menambahkan mailto

--- js/main.js
+++ js/main.js
@@ -37,3 +37,19 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
 
     window.location.href = mailtoLink;
 });
+
+
+
+// contact
+function sendEmail(e) {
+    e.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const senderEmail = document.getElementById('sender-email').value;
+    const project = document.getElementById('project').value;
+    const message = document.getElementById('message').value;
+
+    const mailtoLink = `mailto:tentiresida@gmail.com?subject=New Project Inquiry: ${project}&body=Name: ${name}%0D%0A%0D%0AEmail: ${senderEmail}%0D%0A%0D%0AProject: ${project}%0D%0A%0D%0AMessage:%0D%0A${message}`;
+
+    window.location.href = mailtoLink;
+}
